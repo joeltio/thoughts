073af84bd63f0f29ce92635a66127be4b461b07e
joeltio <joeltiojinhon@gmail.com>
2017-11-05 23:14:57 +0800

    Clean up tests in MindTest

--- Thoughts/app/src/test/java/joeltio/thoughts/MindTest.java
+++ Thoughts/app/src/test/java/joeltio/thoughts/MindTest.java
@@ -8,65 +8,37 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 public class MindTest {
+    private Thought createThought(String name) {
+        return new Thought(name, "", new ArrayList<String>(), new Date());
+    }
+
     @Test
     public void getAllThoughtsReturnsAllThoughts() {
-        ArrayList<String> tags = new ArrayList<>();
-        tags.add("a");
-        tags.add("b");
-
-        Thought thought1 = new Thought("a", "", tags, new Date());
-        Thought thought2 = new Thought("b", "", tags, new Date());
-        Thought thought3 = new Thought("c", "", tags, new Date());
-
         Mind mind = new Mind();
+        Thought thought1 = createThought("a");
+        Thought thought2 = createThought("b");
+
         mind.addThought(thought1);
         mind.addThought(thought2);
-        mind.addThought(thought3);
 
         ArrayList<Thought> thoughts = mind.getAllThoughts();
         assertTrue(thoughts.contains(thought1));
         assertTrue(thoughts.contains(thought2));
-        assertTrue(thoughts.contains(thought3));
-    }
-
-    @Test
-    public void getAllThoughtsReturnsCopyOfAllThoughts() {
-        ArrayList<String> tags = new ArrayList<>();
-        tags.add("a");
-        tags.add("b");
-
-        Thought thought1 = new Thought("a", "", tags, new Date());
-        Thought thought2 = new Thought("b", "", tags, new Date());
-        Thought thought3 = new Thought("c", "", tags, new Date());
-
-        Mind mind = new Mind();
-        mind.addThought(thought1);
-        mind.addThought(thought2);
-        mind.addThought(thought3);
-
-        ArrayList<Thought> thoughts = mind.getAllThoughts();
-        thoughts.clear();
-
-        assertTrue(mind.getAllThoughts().contains(thought1));
     }
 
     @Test
     public void containsReturnsTrueForContainedThoughts() {
-        ArrayList<String> tags = new ArrayList<>();
-        tags.add("a");
-        tags.add("b");
-
-        Thought thought1 = new Thought("a", "", tags, new Date());
-        Thought thought2 = new Thought("a", "abc", tags, new Date());
-        Thought thought3 = new Thought("b", "", tags, new Date());
-        Thought thought4 = new Thought("c", "", tags, new Date());
-
         Mind mind = new Mind();
+
+        Thought thought1 = new Thought("a", "", new ArrayList<String>(), new Date());
+        Thought thought2 = new Thought("a", "abc", new ArrayList<String>(), new Date());
         mind.addThought(thought1);
 
         assertTrue(mind.contains(thought1));
         assertFalse(mind.contains(thought2));
 
+        Thought thought3 = new Thought("b", "", new ArrayList<String>(), new Date());
+        Thought thought4 = new Thought("c", "", new ArrayList<String>(), new Date());
         mind.addThought(thought3);
 
         assertTrue(mind.contains(thought3));
