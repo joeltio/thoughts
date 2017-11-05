bf93a78fc6fe11a872da1ca3b9219a66d0ca28fa
joeltio <joeltiojinhon@gmail.com>
2017-11-05 21:24:54 +0800

    Add Thoughts class that passes tests

---
+++ Thoughts/app/src/main/java/joeltio/thoughts/Thought.java
@@ -0,0 +1,43 @@
+package joeltio.thoughts;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Thought {
+    private String name;
+    private String body;
+    private ArrayList<String> tags;
+    private Date creationDate;
+
+    public Thought(String name, String body, ArrayList<String> tags, Date creationDate) {
+        this.name = name;
+        this.body = body;
+        this.tags = tags;
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Thought &&
+                this.name.equals(((Thought) obj).getName()) &&
+                this.body.equals(((Thought) obj).getBody()) &&
+                this.tags.equals(((Thought) obj).getTags()) &&
+                this.creationDate.equals(((Thought) obj).getCreationDate());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public ArrayList<String> getTags() {
+        return this.tags;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+}
