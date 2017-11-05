e72fb38fd52eb8b5802c61bf1f6a412f119d3d0a
joeltio <joeltiojinhon@gmail.com>
2017-11-05 21:56:29 +0800

    Add tests for getAllThoughts for Mind

--- Thoughts/app/src/test/java/joeltio/thoughts/MindTest.java
+++ Thoughts/app/src/test/java/joeltio/thoughts/MindTest.java
@@ -9,6 +9,48 @@ import static org.junit.Assert.*;
 
 public class MindTest {
     @Test
+    public void getAllThoughtsReturnsAllThoughts() {
+        ArrayList<String> tags = new ArrayList<>();
+        tags.add("a");
+        tags.add("b");
+
+        Thought thought1 = new Thought("a", "", tags, new Date());
+        Thought thought2 = new Thought("b", "", tags, new Date());
+        Thought thought3 = new Thought("c", "", tags, new Date());
+
+        Mind mind = new Mind();
+        mind.addThought(thought1);
+        mind.addThought(thought2);
+        mind.addThought(thought3);
+
+        ArrayList<Thought> thoughts = mind.getAllThoughts();
+        assertTrue(thoughts.contains(thought1));
+        assertTrue(thoughts.contains(thought2));
+        assertTrue(thoughts.contains(thought3));
+    }
+
+    @Test
+    public void getAllThoughtsReturnsCopyOfAllThoughts() {
+        ArrayList<String> tags = new ArrayList<>();
+        tags.add("a");
+        tags.add("b");
+
+        Thought thought1 = new Thought("a", "", tags, new Date());
+        Thought thought2 = new Thought("b", "", tags, new Date());
+        Thought thought3 = new Thought("c", "", tags, new Date());
+
+        Mind mind = new Mind();
+        mind.addThought(thought1);
+        mind.addThought(thought2);
+        mind.addThought(thought3);
+
+        ArrayList<Thought> thoughts = mind.getAllThoughts();
+        thoughts.clear();
+
+        assertTrue(mind.getAllThoughts().contains(thought1));
+    }
+
+    @Test
     public void containsReturnsTrueForContainedThoughts() {
         ArrayList<String> tags = new ArrayList<>();
         tags.add("a");
