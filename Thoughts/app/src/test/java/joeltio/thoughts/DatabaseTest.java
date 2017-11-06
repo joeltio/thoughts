5e9aca73ccdf107c17c61f8c4b15b618b6dfc0df
joeltio <joeltiojinhon@gmail.com>
2017-11-06 19:09:13 +0800

    Add test for deleting Thoughts from Database

--- Thoughts/app/src/test/java/joeltio/thoughts/DatabaseTest.java
+++ Thoughts/app/src/test/java/joeltio/thoughts/DatabaseTest.java
@@ -70,4 +70,28 @@ public class DatabaseTest {
         thought1.setId(2);
         assertFalse(mind.getAllThoughts().contains(thought1));
     }
+
+    @Test
+    public void deleteThoughtRemovesThought() {
+        Thought thought1 = createThought("a");
+        thought1.setId(1);
+        Thought thought2 = createThought("b");
+        thought2.setId(2);
+
+        dbAdapter.insertThought(thought1);
+        dbAdapter.insertThought(thought2);
+
+        assertEquals(2, dbAdapter.getMind().getAllThoughts().size());
+        dbAdapter.removeThought(thought1.getId());
+        Mind mind = dbAdapter.getMind();
+        assertEquals(1, mind.getAllThoughts().size());
+        assertEquals(thought2, mind.getAllThoughts().get(0));
+
+        DbAdapter dbAdapter2 = new DbAdapter(RuntimeEnvironment.application);
+        dbAdapter2.open();
+        mind = dbAdapter2.getMind();
+        assertEquals(1, mind.getAllThoughts().size());
+        assertEquals(thought2, mind.getAllThoughts().get(0));
+        dbAdapter2.close();
+    }
 }
