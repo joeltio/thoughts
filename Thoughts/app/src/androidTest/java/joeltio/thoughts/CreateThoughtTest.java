1b8a038987d8e813227dc6e1ada231a0d05a9744
joeltio <joeltiojinhon@gmail.com>
2017-11-08 12:18:37 +0800

    Add UI test to check saving of thoughts

---
+++ Thoughts/app/src/androidTest/java/joeltio/thoughts/CreateThoughtTest.java
@@ -0,0 +1,54 @@
+package joeltio.thoughts;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class CreateThoughtTest {
+    @Rule
+    public ActivityTestRule<CreateThoughtActivity> activityRule = new ActivityTestRule<>(CreateThoughtActivity.class);
+
+    @Before
+    @After
+    public void clearDatabase() {
+        DbAdapter dbAdapter = new DbAdapter(activityRule.getActivity());
+        dbAdapter.open();
+        dbAdapter.reset();
+        dbAdapter.close();
+    }
+
+    @Test
+    public void activitySavesThoughtsIntoDatabase() {
+        String thoughtName = "Test thought 1";
+        String thoughtBody = "The quick brown fox jumped over the fence.";
+        onView(withId(R.id.thought_name_field)).perform(typeText(thoughtName));
+        onView(withId(R.id.thought_body_field)).perform(typeText(thoughtBody));
+        onView(withId(R.id.thought_tags_field)).perform(typeText("tag1"));
+        onView(withId(R.id.action_thought_done)).perform(click());
+
+        DbAdapter dbAdapter = new DbAdapter(activityRule.getActivity());
+        dbAdapter.open();
+        Mind mind = dbAdapter.getMind();
+        dbAdapter.close();
+
+        assertEquals(1, mind.getAllThoughts().size());
+        Thought thought = mind.getAllThoughts().get(0);
+        assertEquals(thoughtName, thought.getName());
+        assertEquals(thoughtBody, thought.getBody());
+        assertEquals(1, thought.getTags().size());
+        assertTrue(thought.getTags().contains("tag1"));
+    }
+}
